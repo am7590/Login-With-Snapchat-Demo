78a55627a5d23ed30c2ae9326e6f2bd5052c1203
Alek Michelson <am7590@rit.edu>
2022-10-23 15:32:33 -0400

    Initial Commit

---
+++ SnapchatLoginDemo/SnapchatLoginDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapchatLoginDemoApp.swift
+//  SnapchatLoginDemo
+//
+//  Created by Alek Michelson on 10/23/22.
+//
+
+import SwiftUI
+
+@main
+struct SnapchatLoginDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
