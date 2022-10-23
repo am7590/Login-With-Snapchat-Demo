9809e34d1a87936b41fc27b05c8fefc9bf21907e
Alek Michelson <am7590@rit.edu>
2022-10-23 16:44:42 -0400

    Add AppDelegate

--- SnapchatLoginDemo/SnapchatLoginDemoApp.swift
+++ SnapchatLoginDemo/SnapchatLoginDemoApp.swift
@@ -6,12 +6,29 @@
 //
 
 import SwiftUI
+import UIKit
+import SCSDKLoginKit
 
 @main
 struct SnapchatLoginDemoApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        print("didFinishLaunchingWithOptions")
+        return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        
+        return SCSDKLoginClient.application(app, open: url, options: options)
     }
 }
